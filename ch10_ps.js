34d23cc1bc43c87406d4acd0b7d12e6212747428
Bunny <prajwalmhaskar28@gmail.com>
2025-01-08 19:02:12 +0530

    implemented json and stylings with api dataset

--- ch10_ps.js
+++ ch10_ps.js
@@ -2,35 +2,22 @@
 // response.json() allows you to take the response body from a network request (such as a fetch() request) and parse it into a JavaScript object if the response body is in JSON format.
 // Since JSON parsing is an asynchronous operation (it might take some time), response.json() returns a Promise that resolves with the parsed data once the parsing is complete.
 
-let p = fetch('https://jsonplaceholder.typicode.com/posts')
+let p = fetch('freecarapi.json')
+
+let v = document.body.getElementsByTagName("img")
+// console.log(v)
+let imgText = document.getElementsByClassName("image_Data_text");
 
 const abc = async () => {
-
-    let v = document.body.getElementsByClassName("square")
-    console.log(v)
-
-    p.then((response) => response.json()
-    )
+    await p.then((response) => response.json())
         .then((user) => {
-            console.log(user[0]);
-            return user
-            // document.body.innerHTML = JSON.stringify(json[0])
+            for (i = 0; i < v.length; i++) {
+                console.log(user[i]);
+            }
+            for (let i = 0; i < imgText.length; i++) {
+                    imgText[i].innerHTML = user[i].id + " " + user[i].make + " " + user[i].model
+            }
         })
-        .then(
-            // (a) =>
-            //document.body.innerHTML = JSON.stringify(a[0])
-            (a) => {
-                console.log(a[1])
-                let i = 0
-                for (i; i < 12; i++) {
-                    //console.log(i+" before")
-                    v[i].innerHTML = JSON.stringify(a[i].title)
-                    //console.log(i+" after")
-                }
-                //console.log(i)
-            })
-            //.catch((e)=>console.log(e.stack))
-
 }
 
 abc()
